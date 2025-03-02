2b88c4da89a1e63ec3a047619da336fff170a29d
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-03-02 12:37:06 +0100

    oprava

--- product_technical_properties/models/product_template.py
+++ product_technical_properties/models/product_template.py
@@ -1,39 +1,12 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
 
 class ProductTemplate(models.Model):
-    _name = 'product.template'
     _inherit = 'product.template'
-
-    # Hodnoty kondenzátoru
-    ptp_cap_part_number = fields.Char(string="Capacitor Part Number", required=True)
-    ptp_cap_value = fields.Float(string="Capacitor Value", required=True)
-    ptp_cap_unit = fields.Char( string="Capacitor Unit", required=True)
-    ptp_cap_full_value = fields.Char(string="Capacitor Full Value", compute="_compute_full_values", store=True)
-    ptp_cap_voltage_rating = fields.Float(string="Capacitor Voltage Rating [VDC]", required=True)
-    ptp_cap_dielectric = fields.Char( string="Capacitor Dielectric", required=True)
-    ptp_cap_tolerance = fields.Float(string="Capacitor Tolerance [%]")
-    ptp_cap_footprint = fields.Char( string="Capacitor Footprint", required=True)
-    ptp_cap_note = fields.Text(string="Capacitor Note")
-
-    # Hodnoty rezistoru
-    ptp_res_part_number = fields.Char(string="Resistor Part Number", required=True)
-    ptp_res_value = fields.Float(string="Resistor Value", required=True)
-    ptp_res_unit = fields.Char(string="Resistor Unit", required=True)
-    ptp_res_full_value = fields.Char(string="Resistor Full Value", compute="_compute_full_values", store=True)
-    ptp_res_power_rating = fields.Char(string="Resistor Power Rating", required=True)
-    ptp_res_tolerance = fields.Float(string="Resistor Tolerance [%]", required=True)
-    ptp_res_voltage_rating = fields.Float(string="Resistor Voltage Rating [V]", required=True)
-    ptp_res_footprint = fields.Char( string="Resistor Footprint", required=True)
-    ptp_res_note = fields.Text(string="Resistor Note")
-
-    # Hodnoty dalších komponent
-    ptp_other_part_number = fields.Char(string="Other Component Part Number", required=True)
-    ptp_other_note = fields.Text(string="Other Component Note")
-
+    
+    ptp_cap_unit = fields.Many2one('ptp.unit', string='Capacitor Unit')
+    ptp_res_unit = fields.Many2one('ptp.unit', string='Resistor Unit')
+    
     @api.depends('ptp_cap_value', 'ptp_cap_unit', 'ptp_res_value', 'ptp_res_unit')
     def _compute_full_values(self):
         for record in self:
-            record.ptp_cap_full_value = f"{record.ptp_cap_value} {record.ptp_cap_unit}" if record.ptp_cap_value and record.ptp_cap_unit else ''
-            record.ptp_res_full_value = f"{record.ptp_res_value} {record.ptp_res_unit}" if record.ptp_res_value and record.ptp_res_unit else ''
+            record.ptp_res_full_value = f"{record.ptp_res_value} {record.ptp_res_unit.name}" if record.ptp_res_value and record.ptp_res_unit else ''
