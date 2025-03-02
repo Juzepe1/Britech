0762fb10f461945110b438ee5df286d7fb3bd919
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-03-02 12:18:31 +0100

    oprava

--- product_technical_properties/models/__init__.py
+++ product_technical_properties/models/__init__.py
@@ -2,4 +2,3 @@
 
 from . import product_category
 from . import product_template
-from . import ptp_unit
--- product_technical_properties/models/ptp_unit.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class PtpUnit(models.Model):
-    _name = 'ptp.unit'
-    _description = 'Technical Unit'
-    
-    name = fields.Char(string='Unit Name', required=True, translate=True)
