831707391ce1e57e710374f6740942fe20bf25a3
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-03-02 10:41:33 +0100

    oprava

---
+++ product_technical_properties/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Product Technical Properties',
+    'version': '1.0',
+    'category': 'Product',
+    'summary': 'Adds technical properties fields to products',
+    'depends': ['product'],
+    'data': [
+        'views/product_template_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
