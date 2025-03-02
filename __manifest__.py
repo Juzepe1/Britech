76e7010e723d3ae3e851b9179bcf862c8efe1c37
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-03-02 10:14:34 +0100

    init

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Product Properties Simplified',
+    'version': '1.3',
+    'category': 'Product',
+    'summary': 'Adds technical properties to products without extra scripts',
+    'author': 'Custom Developer',
+    'depends': ['product'],
+    'data': [
+        'views/product_property_views.xml',
+    ],
+    'installable': True,
+    'application': False
+}
