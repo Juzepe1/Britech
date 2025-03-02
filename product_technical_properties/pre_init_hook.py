b806bcfa99b0085c6a39dd62a5409f18869ff750
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-03-02 11:43:33 +0100

    oprava

--- product_technical_properties/pre_init_hook.py
+++ product_technical_properties/pre_init_hook.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 
-def pre_init_hook(cr):
+def pre_init_hook(cr_or_env):
     """Kompletně odstraní konfliktní výběrové hodnoty v ir.model.fields a ir.model.fields.selection."""
+
+    # Detekce, zda byl hook zavolán s `cr` nebo `env`, a získání `cr`
+    if hasattr(cr_or_env, 'cr'):  # Pokud je `cr_or_env` environment, získáme `cr`
+        cr = cr_or_env.cr
+    else:
+        cr = cr_or_env  # Pokud je to přímo `cr`, použijeme ho
+
     cr.execute("""
         DELETE FROM ir_model_fields_selection
         WHERE field_id IN (
