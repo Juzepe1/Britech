3aa2fa25ca74a28c4d2bfd1b7e00d136f2e128b8
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-03-02 11:46:28 +0100

    oprava

--- product_technical_properties/__init__.py
+++ product_technical_properties/__init__.py
@@ -2,3 +2,4 @@
 
 from . import models
 from .pre_init_hook import pre_init_hook
+from .post_init_hook import post_init_hook
--- product_technical_properties/__manifest__.py
+++ product_technical_properties/__manifest__.py
@@ -11,4 +11,5 @@
     'installable': True,
     'application': False,
     'pre_init_hook': 'pre_init_hook',
+    'post_init_hook': 'post_init_hook',
 }
