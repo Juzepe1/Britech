b78f6b7d34d26da77c52b08e77c4d90aaefc7bc4
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-03-16 18:05:54 +0100

    Update product_template.py

--- product_technical_parameters_systee/models/product_template.py
+++ product_technical_parameters_systee/models/product_template.py
@@ -82,7 +82,7 @@ class ProductTemplate(models.Model):
 
             # Zjistíme, jaká pole jsou pro kategorii relevantní
             category_type = rec.categ_id.ptp_systee_component_type
-            ptp_fields = [field for field in rec._fields if field.startswith('ptp_systee_') and not field.endswith('_combined' and not field.endswith('_related' and not field.endswith('_note')]
+            ptp_fields = [field for field in rec._fields if field.startswith('ptp_systee_') and not field.endswith('_combined') and not field.endswith('_related') and not field.endswith('_note')]
 
             # Seznam hodnot, které mají být spojeny
             value_parts = []
