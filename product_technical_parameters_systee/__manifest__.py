b4cf6c202bab01214a489b475c25d6541924fbb1
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-03-18 21:04:39 +0100

    uprava manifestu

--- product_technical_parameters_systee/__manifest__.py
+++ product_technical_parameters_systee/__manifest__.py
@@ -36,7 +36,7 @@
 
 {
     'name': 'Product Technical Parameters - Systee',
-    'version': '18.0.0.0.1',
+    'version': '18.0.0.0.2',
     'summary': "Rozšíření product.category o volitelný typ součástky a úprava product.template.",
     'author': 'Systee s.r.o. (https://www.systee.cz)',
     'license': 'Other proprietary',
@@ -47,6 +47,7 @@
     'depends': [
         'product',
         'base',
+        'sale',
     ],
     'data': [
         'security/ir.model.access.csv',
