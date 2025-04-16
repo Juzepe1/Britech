00881226ad253841153388f8535777c90c0f0ec8
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-04-16 17:56:41 +0200

    Update product_template.py

--- product_technical_parameters_systee/models/product_template.py
+++ product_technical_parameters_systee/models/product_template.py
@@ -570,39 +570,39 @@ class ProductTemplate(models.Model):
             rec.ptp_zen_vz_full_value = f"{value}{unit}".strip()
             
             for rec in self:
-            combined = []
+                combined = []
 
-            for field_name, field in rec._fields.items():
-                # Zajímá nás jen "ptp_" prefix, ale vynecháme pár výjimek
-                if not field_name.startswith('ptp_'):
-                    continue
-                if field_name in ('ptp_value_unit_combined', 'ptp_part_number', 'ptp_note', 'ptp_footprint', 'ptp_sequence_number'):
-                    continue
-                if field_name.endswith('_full_value'):
-                    continue
+                for field_name, field in rec._fields.items():
+                    # Zajímá nás jen "ptp_" prefix, ale vynecháme pár výjimek
+                    if not field_name.startswith('ptp_'):
+                        continue
+                    if field_name in ('ptp_value_unit_combined', 'ptp_part_number', 'ptp_note', 'ptp_footprint', 'ptp_sequence_number'):
+                        continue
+                    if field_name.endswith('_full_value'):
+                        continue
 
-                val = getattr(rec, field_name, False)
-                if val in (None, False, '', 0):
-                    continue
+                    val = getattr(rec, field_name, False)
+                    if val in (None, False, '', 0):
+                        continue
 
-                display_value = ''
-                try:
-                    if isinstance(field, fields.Many2one):
-                        # Bezpečný přístup na .name
-                        if isinstance(val, models.BaseModel):
-                            display_value = val.name or ''
-                    elif isinstance(field, (fields.Char, fields.Text)):
-                        display_value = str(val).strip()
-                    elif isinstance(field, (fields.Integer, fields.Float)):
-                        display_value = str(val)
-                except Exception:
-                    # Bezpečnostní fallback
-                    continue
+                    display_value = ''
+                    try:
+                        if isinstance(field, fields.Many2one):
+                            # Bezpečný přístup na .name
+                            if isinstance(val, models.BaseModel):
+                                display_value = val.name or ''
+                        elif isinstance(field, (fields.Char, fields.Text)):
+                            display_value = str(val).strip()
+                        elif isinstance(field, (fields.Integer, fields.Float)):
+                            display_value = str(val)
+                    except Exception:
+                        # Bezpečnostní fallback
+                        continue
 
-                if display_value:
-                    combined.append(display_value)
+                    if display_value:
+                        combined.append(display_value)
 
-            rec.ptp_value_unit_combined = ' '.join(combined) if combined else False
+                rec.ptp_value_unit_combined = ' '.join(combined) if combined else False
     # --------------------------------------------------------------------------------
     # Metoda pro opravu . za , u typu char
     # --------------------------------------------------------------------------------
